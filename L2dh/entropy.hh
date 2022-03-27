3d19462e545c7da6bc7780e16d4a79576b01c160
JanuszWitkowski <wicman360@gmail.com>
2022-03-27 22:19:36 +0200

    Start dynamic huffman

---
+++ L2dh/entropy.hh
@@ -0,0 +1,20 @@
+#ifndef __ENTROPY_H
+#define __ENTROPY_H
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <math.h>
+using namespace std;
+typedef unsigned char uchar;
+
+uchar* fileToArray (string filename, int &n);
+
+int* initCharOccs (int value);
+int* countCharOccs (uchar* array, int n);
+int** countCharOccsCond (uchar* array, int n);
+
+double entropy (int* charOccs);
+double entropyCond (int** charOccsCond);
+
+#endif
