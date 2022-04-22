575c1d4c63af50a5a2585510d084eb9cc295f57e
JanuszWitkowski <wicman360@gmail.com>
2022-04-22 20:00:09 +0200

    Initial commit for L4

---
+++ L4/bitrw.hh
@@ -0,0 +1,69 @@
+#ifndef __BITRW_H
+#define __BITRW_H
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <math.h>
+using namespace std;
+
+typedef unsigned char uchar;
+// typedef uint8_t uchar;
+
+class BitHandler {
+protected:
+    uchar byteBuffer;
+    size_t byteBufferIndex;
+public:
+    BitHandler();
+};
+
+class BitReader : public BitHandler {
+private:
+    uchar* file;
+    size_t fileSize;
+    size_t fileIndex;
+public:
+    BitReader(string filename);
+    virtual ~BitReader (void);
+    uchar getNextByte();
+    bool isNextBitOne();
+    uint getNextBit();
+    void printArray();
+
+    uint getCurrentBit () {
+        return (byteBuffer >> byteBufferIndex) & 1;
+    }
+
+    uchar getByte (size_t index) {
+        return file[index];
+    }
+
+    uchar* getFileArray () {
+        return file;
+    }
+
+    size_t getFileSize () {
+        return fileSize;
+    }
+};
+
+class BitWriter : public BitHandler {
+private:
+    ofstream file;
+    void clearBuffer();
+public:
+    BitWriter(string filename);
+    virtual ~BitWriter (void);
+    void writeBit(int bit);
+    void writeByte(uchar byte);
+    void writeString(string text);
+    void padWithZeros();
+    void padWithZerosByte();
+    void closeFile();
+};
+
+uchar* fileToArray (string filename, size_t &n);
+void printArray (uchar* array, size_t n);
+
+#endif
---
+++ L4/stats.hh
@@ -0,0 +1,23 @@
+#ifndef __STATS_H
+#define __STATS_H
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <math.h>
+using namespace std;
+typedef unsigned char uchar;
+// typedef uint8_t uchar;
+
+void fillOccs (int* occs, int value);
+int* initCharOccs (int value);
+int* countCharOccs (uchar* array, int n);
+int** countCharOccsCond (uchar* array, int n);
+
+double entropy (int* charOccs, size_t n);
+double entropyCond (int* charOccs, int** charOccsCond, size_t n);
+
+double compressionRate (size_t fileSize, size_t codeSize);
+double avgCodeLength (size_t fileSize, size_t codeSize);
+
+#endif
