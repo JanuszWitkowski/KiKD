e2d5362686af5ac2be502b1b1943980e0607c54c
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2022-05-21 19:06:45 +0200

    Add colors to error messegaes

--- L5/main.cc
+++ L5/main.cc
@@ -16,15 +16,15 @@ int integerPower (int e) {
 
 int main (int argc, char* argv[]) {
     if (argc != 4) {
-        cout << "BŁĄD: Zła liczba argumentów." << endl;
+        cout << cRed << "BŁĄD: Zła liczba argumentów." << endl;
         cout << "Wywołanie programu powinno wyglądać następująco:" << endl;
-        cout << "./main [nazwa pliku wejściowego] [nazwa pliku wyjściowego] [wykładnik liczby kolorów 0-24]" << endl;
+        cout << "./main [nazwa pliku wejściowego] [nazwa pliku wyjściowego] [wykładnik liczby kolorów 0-24]" << cReset << endl;
     } else {
         string inName = argv[1], outName = argv[2];
         int colorsNumberExp = stoi(argv[3]);
         if  (colorsNumberExp < 0 || colorsNumberExp > 24) {
-            cout << "BŁĄD: Niepoprawny parametr liczby kolorów." << endl;
-            cout << "Dany parametr powinien przyjąć liczbę całkowitą z zakresu [0, 24]." << endl;
+            cout << cRed << "BŁĄD: Niepoprawny parametr liczby kolorów." << endl;
+            cout << "Dany parametr powinien przyjąć liczbę całkowitą z zakresu [0, 24]." << cReset << endl;
         } else {
             size_t n;
             uchar* inFile = fileToArray(inName, n), outFile;
